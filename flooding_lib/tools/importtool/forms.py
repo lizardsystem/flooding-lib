265a3bbd9600901e850f8b7d50a02e2b474e74b4
Remco Gerlich <remco@gerlich.nl>
2012-06-20 10:34:19 +0200

    pep8

--- flooding_lib/tools/importtool/forms.py
+++ flooding_lib/tools/importtool/forms.py
@@ -1,67 +1,27 @@
 from django import forms
-from flooding_lib.tools.importtool.models import ImportScenario, GroupImport
-from flooding_lib.models import Breach, Project, Scenario, SobekModel, SobekVersion
+from flooding_lib.tools.importtool.models import GroupImport
 
-class ImportScenarioForm(forms.ModelForm):
-    """
-        niet meer in gebruik
-    """
-    class Meta:
-        model = ImportScenario
-        exclude = ('state', 'action_taker', 'creation_date', 'project', 'breach', 'validation_remarks')
-    description = forms.CharField(widget = forms.widgets.Textarea(attrs={'rows':1, 'cols':60}) )
-    coordinates_breach_location = forms.CharField(widget = forms.widgets.Textarea(attrs={'rows':1, 'cols':60}) )
-    remarks = forms.CharField(widget = forms.widgets.Textarea(attrs={'rows':1, 'cols':60}) )
-
-class VerifyScenarioForm(forms.ModelForm):
-    """
-        niet meer in gebruik
-    """
-    class Meta:
-        model = ImportScenario
-        exclude = ('action_taker',
-                   'creation_date',
-                   'animation_flooding_file',
-                   'max_depth_maps_file',
-                   'max_flow_maps_file',
-                   'arrival_times_map_file',
-                   'animation_flow_file')
-
-    description = forms.CharField(widget = forms.widgets.Textarea(attrs={'rows':1, 'cols':60}))
-    coordinates_breach_location = forms.CharField(widget = forms.widgets.Textarea(attrs={'rows':1, 'cols':60}) )
-
-    empty_choice = [(None, '----------')]
-
-    project_choices = empty_choice +  [(project.id, project.name) for project in Project.objects.all()]
-    scenario_choices = empty_choice +  [(scenario.id, scenario.name) for scenario in Scenario.objects.all()]
-    breach_choices = empty_choice +  [(breach.id, breach.name) for breach in Breach.objects.all()]
-    sobek_model_choices = empty_choice +  [(sobek_model.id, sobek_model.sobekmodeltype) for sobek_model in SobekModel.objects.all()]
-    sobek_version_choices = empty_choice +  [(sobek_version.id, sobek_version.name) for sobek_version in SobekVersion.objects.all()]
-
-    project_select = forms.ChoiceField(choices=project_choices)
-    scenario_select = forms.ChoiceField(choices=scenario_choices)
-    breach_select = forms.ChoiceField(choices=breach_choices)
-    sobek_model_select = forms.ChoiceField(choices=sobek_model_choices)
-    sobek_version_select = forms.ChoiceField(choices=sobek_version_choices)
 
 class GroupImportForm(forms.ModelForm):
-    """
-        import form for group imports
-    """
+    """import form for group imports"""
     class Meta:
         model = GroupImport
         exclude = ('upload_successful')
 
+
 class ImportScenarioFileUploadForm(forms.Form):
-    """ The form for uploading files for an import scenario
+    """The form for uploading files for an import scenario"""
 
-    """
-
-    def __init__(self, filefields, *args,**kwargs):
-        forms.Form.__init__(self, *args,**kwargs)
+    def __init__(self, filefields, *args, **kwargs):
+        forms.Form.__init__(self, *args, **kwargs)
         if filefields:
             for f in filefields:
                 if type(f) == type(u" "):
-                    self.fields.insert(0, f, forms.FileField(label = f, required=False, widget = forms.widgets.FileInput))
+                    self.fields.insert(0, f, forms.FileField(
+                            label=f, required=False,
+                            widget=forms.widgets.FileInput))
                 else:
-                    self.fields.insert(0, f.name, forms.FileField(label = f.name, required=False, widget = forms.widgets.FileInput(attrs={'size':'80'})))
+                    self.fields.insert(0, f.name, forms.FileField(
+                            label=f.name, required=False,
+                            widget=forms.widgets.FileInput(
+                                attrs={'size': '80'})))
