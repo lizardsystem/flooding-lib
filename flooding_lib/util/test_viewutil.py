f623431e9e4cdc357dfc3dcd2356f4566a75ea9d
Remco Gerlich <remco@gerlich.nl>
2012-08-17 09:45:14 +0200

    Forgot to add the flooding_lib.util.viewutil module, where I moved a bit of the views.py code.

---
+++ flooding_lib/util/test_viewutil.py
@@ -0,0 +1,11 @@
+# Python 3 is coming to town
+from __future__ import print_function, unicode_literals
+from __future__ import absolute_import, division
+
+from django.test import TestCase
+from flooding_lib.util import viewutil
+
+
+#class TestServeFile(TestCase):
+#    def test_trivial(self):
+#        viewutil.serve_file(None, None, None, None)
---
+++ flooding_lib/util/viewutil.py
@@ -0,0 +1,44 @@
+"""Utils for use in views."""
+
+# Python 3 is coming to town
+from __future__ import print_function, unicode_literals
+from __future__ import absolute_import, division
+
+import os
+import platform
+
+from django.conf import settings
+from django import http
+from django.views import static
+
+
+def serve_file(request, dirname, filename, nginx_dirname, debug=None):
+    """Serve a static file. If running under Linux and not in debug
+    mode, let Apache or Nginx serve it, otherwise use Django's static
+    serve view."""
+
+    # It's also possible to pass it in for easy testing
+    if debug is None:
+        debug = settings.DEBUG
+
+    response = http.HttpResponse()
+
+    # Unset the Content-Type as to allow for the webserver
+    # to determine it.
+    response['Content-Type'] = ''
+
+    # Apache
+    response['X-Sendfile'] = os.path.join(dirname, filename)
+
+    # Nginx
+    nginx_path = os.path.join(nginx_dirname, filename)
+    response['X-Accel-Redirect'] = nginx_path
+
+    # Sending by webserver only works for Apache and Nginx, under
+    # Linux right now. Otherwise and in development, use Django's
+    # static serve.
+    if debug or not platform.system() == 'Linux':
+        return static.serve(
+            request, os.path.join(dirname, filename), '/')
+
+    return response
