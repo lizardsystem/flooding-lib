c67e33ab75c8a0959dfe9d42622e4fe643b811cc
Remco Gerlich <remco@gerlich.nl>
2014-08-29 16:18:59 +0200

    Remove $

--- flooding_lib/tools/exporttool/urls.py
+++ flooding_lib/tools/exporttool/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns(
         'flooding_lib.tools.exporttool.views.exportrun_resultfile',
         name='flooding_tools_export_resultfile'),
 
-    url(r'^exportdetail/(?P<export_run_id>\d+)$',
+    url(r'^exportdetail/(?P<export_run_id>\d+)/',
         'flooding_lib.tools.exporttool.views.export_detail',
         name='flooding_tools_export_detail'),
 
