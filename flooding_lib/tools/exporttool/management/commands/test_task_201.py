5b656cc315a6f8864aa86a59053caf527b30a0ad
Alexandr Seleznev <a.seleznev@hotmail.nl>
2014-11-13 16:47:50 +0100

    flooding_lib/tools/exporttool/management/commands/test_task_201.py

---
+++ flooding_lib/tools/exporttool/management/commands/test_task_201.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+from flooding_lib.tasks import calculate_export_maps
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        print 'Running ExportRun with id %s...' % args[0]
+        calculate_export_maps.calculate_export_maps(args[0])
