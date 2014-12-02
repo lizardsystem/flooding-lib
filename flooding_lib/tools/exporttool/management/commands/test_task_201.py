53c2770ccf1791fa18945ab803c4be4af4b663ea
Remco Gerlich <remco@gerlich.nl>
2014-12-02 12:03:30 +0100

    Call right function for task 201

--- flooding_lib/tools/exporttool/management/commands/test_task_201.py
+++ flooding_lib/tools/exporttool/management/commands/test_task_201.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
 
-from flooding_lib.tasks import calculate_export_maps
+from flooding_lib.tasks import calculate_export_data
+
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         print 'Running ExportRun with id %s...' % args[0]
-        calculate_export_maps.calculate_export_maps(args[0])
+        calculate_export_data.calculate_export_data(args[0])
