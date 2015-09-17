3a1c193c22c28cb532337100987c9a886cf1e5cf
Alexandr Seleznev <a.seleznev@hotmail.nl>
2015-09-17 16:59:51 +0200

    Write output to file.

--- flooding_lib/management/commands/check_zip_files.py
+++ flooding_lib/management/commands/check_zip_files.py
@@ -24,7 +24,7 @@ class Command(BaseCommand):
                                format(path))
         self.walk_zips(path)
 
-    def test_zips(self, zippathes):
+    def test_zips(self, zippathes, out):
         count = 0
         for zippath in zippathes:
             try:
@@ -33,16 +33,18 @@ class Command(BaseCommand):
                     result = zip.testzip()
                 if result:
                     count += 1
-                    print('First corrupted file "%s" in zip "%s"' % (
-                        result, zipzippzth))
+                    out.write('First corrupted file "%s" in zip: "%s\n"' % (
+                        result, zippath))
             except:
                 count += 1
-                print("Corrupted zip: '%s'." % zippath)
+                out.write("Corrupted zip: '%s'\n" % zippath)
         return count
 
     def walk_zips(self, path):
         if os.path.isdir(path):
             walk = os.walk(path)
+        out = open('/tmp/resultaten_check_zips.txt', 'wb')
+        out.write("Start on path: %s.\n" % path)
         print("Start test.")
         count = 0
         count_corrupted = 0
@@ -51,5 +53,10 @@ class Command(BaseCommand):
                 path_patern = os.path.join(root, dir, '*.zip'.encode('UTF-8'))
                 zips = glob.glob(os.path.join(path_patern))
                 count += len(zips)
-                count_corrupted += self.test_zips(zips)
-        print("End test, total: %d, corrupted: %d" % (count, count_corrupted))
+                count_corrupted += self.test_zips(zips, out)
+        out.write("End test, total: %d, corrupted: %d\n" % (
+                count, count_corrupted))
+        out.close()
+        print("End test, total: %d, corrupted: %d" % (
+                count, count_corrupted))
+        print("Result is written to /tmp/resultaten_check_zips.txt")
